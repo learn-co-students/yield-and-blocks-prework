0e3f17d0f72101ca42cc095f522ecd1c68d621a9
Jonathan Andrews <jonathanandrewsuk@gmail.com>
2017-02-27 18:28:15 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    array.keep_if{|name| name.start_with?("T", "t")}
+    i = 0
+    while i < array.length
+    yield(array[i])
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
-
-# call your method here!
-
