4590105373c773e9ab7fd9f8ae43d8df5b07a0ef
github username <github email address>
2017-03-21 17:20:15 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,61 @@
-def hello_t
+#def hello_t(array)
+#    new_array=[]
+#    i = 0
+#    while i < array.length
+#       new_array.push yield(array[i])
+#        i = i+1
+#        end
+#    new_array
+#    end
 
+#def hello_t(array)
+    #  if block_given?
+        #  i = 0
+    #  while i < array.length
+        #  yield(array[i])
+        #   i = i + 1
+        # end
+    #array.select do |x|
+            #x.start_with?("T") || x.start_with?("t")
+        #end
+        #    end
+    #end
+
+
+#hello_t(["Tim", "Tom", "Jim"]) do |name|
+#if name.start_with?("T") || name.start_with?("t")
+        #puts "Hi, #{name}"
+    #    end
+#    end
+
+
+def hello_t(array)
+    array.each do |name|
+        if name.start_with?("T")||name.start_with?("t")
+        puts "Hi, #{name}"
+        end
+    end
+    array.select do |x|
+    x.start_with?("T") || x.start_with?("t")
+    end
 end
 
 # call your method here!
+#hello_t(["Tim", "Tom", "Jim"]) do |name|
+    #    if name.start_with?("T") || name.start_with?("t")
+        #puts "Hi, #{name}"
+       # end
+    #end
 
+#def hello_t(array)
+#    if block_given?
+#        i = 0
+#        while i < array.length
+#            yield(array[i])
+#            i = i + 1
+#        end
+#        array
+#        else
+#        puts "Hey! No block was given!"
+#    end
+#end
