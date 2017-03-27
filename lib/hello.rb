69210d119e4c2aabd808f1d042e56b8996cfdb9b
brian-hopman <hopman_2@hotmail.com>
2017-03-26 19:30:29 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def hello_t(array)
+  correct = []
+  array.collect {  |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
+      correct << name
+    elsif name.start_with?("t")
+      puts "Hi, #{name}"
+      correct << name
+  end}
+  return correct
 end
-
-# call your method here!
-
