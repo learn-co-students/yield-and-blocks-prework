ef4583c3ead86d8bceb95f597247a1359c732917
Gaby Stern <stern.gabrielle@gmail.com>
2017-03-30 00:54:01 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,18 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    if array[i].capitalize.start_with?("T")
+      yield(array[i])
+      new_array.push(array[i])
+    end
+      i = i + 1
+  end
+  new_array
 end
 
-# call your method here!
+names = ["Tom", "Terry", "Gaby"]
 
+hello_t(names) do |name|
+  puts "Hi, #{name}"
+end
