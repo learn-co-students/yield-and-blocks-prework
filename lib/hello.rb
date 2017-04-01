f274d3e6d4447cdebc39238a50ab7fb9019ac7c0
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-04-01 21:05:49 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
-
+def hello_t(array)
+  newray = []
+  i = 0
+  while i < array.length
+    if array[i].start_with?("T")
+      yield(array[i])
+      newray.push(array[i])
+    elsif array[i].start_with?("t")
+      yield(array[i])
+      newray.push(array[i])
+    else
+    end
+    i = i + 1
+  end
+return newray
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+   puts "Hi, #{name}"
+ end
