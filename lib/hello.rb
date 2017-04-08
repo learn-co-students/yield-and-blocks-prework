53626fb56b945ae4b0946ee6fb5a6d911b8ef5a9
Luis Arita <luis@ccc-it.net>
2017-04-08 16:56:59 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
-
+def hello_t(array)
+  names_start_with_t = []
+  if block_given?
+    array.each do |name|
+      if name.capitalize.start_with?("T")
+        names_start_with_t.push(name)
+        yield(name)
+      end
+    end
+  else
+    puts "Hey! No block was given!"
+  end
+  names_start_with_t
 end
 
-# call your method here!
-
+#hello_t(["Tim", "Tom", "Jim"]) do |name|
+#    if name.start_with?("T")
+#        puts "Hi, #{name}"
+#    end
+#end
