b866a426e5be06f609e9cbe76f747782311bbfe8
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-03 19:44:54 -0400

    finished lab

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,27 @@
-def hello_t
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i = i + 1
+#   end
+#   array
+# end
+#
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#     if name.start_with?("T")
+#       puts "Hi, #{name}"
+#     end
+# end
 
+def hello_t(array)
+  foo = []
+  array.each do |name|
+    if name.start_with?("T") || name.start_with?("t")
+      yield(name)
+      foo << name
+    end
+  end
+  foo
 end
 
-# call your method here!
-
+#hello_t(["Tim", "Tom", "Jim"]) {"Hi, #{name}"}
