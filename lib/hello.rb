31da6be2d68ea00c8bebd0bbc69c759016c1eb14
James Rhee <jamesshrhee@gmail.com>
2017-05-16 18:17:05 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
-
+def hello_t(arr)
+  result = []
+  arr.each_index do |i|
+    yield(arr[i])
+    result << arr[i] if arr[i].upcase.start_with?("T")
+  end
+  result
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.upcase.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
