3c4ab02f6a67fa03a0c3d2bfa2bc3b8bdb8b317d
Adames Hodelin <adameshh@gmail.com>
2017-06-02 19:13:46 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  return_array = []
+  while i < array.length
+    item = array[i]
+    if item[0].downcase == 't'
+      return_array.push(item)
+      yield(item)
+    end
+    i += 1
+  end
+  return_array
 end
-
-# call your method here!
-
