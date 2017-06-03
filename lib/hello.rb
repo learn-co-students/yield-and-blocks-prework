9626d85e64bff16258766d149d807aa954f36d4c
Sho Miyata <sho.miyata.1@gmail.com>
2017-06-03 18:50:06 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+def hello_t(array)
+  $newarray = []
+  if block_given?
+    i = 0
+    while i < array.length
+      if array[i].upcase.start_with?("T")
+        $newarray.push(array[i])
+        yield(array[i])
+        i += 1
+      else
+        i += 1
+    end
+    end
+  else
+    puts "Hey! No block was given!"
+  end
+  return $newarray
 end
-
 # call your method here!
-
