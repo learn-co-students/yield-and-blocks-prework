71e3889d69a9b8084fb97f18a7efd3c60866eed2
Steven Balasta <sbal13@gmail.com>
2017-06-20 05:45:31 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  t_array = array.select{|x| x.capitalize.start_with?("T")}
+  while i < t_array.length
+    yield t_array[i]
+    i += 1
+  end
+  t_array
 end
 
 # call your method here!
-
