9aa681838e050874338465007227794388de770b
Justin Manalad <jstn.mnld@gmail.com>
2017-07-01 13:45:50 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,24 @@
-def hello_t
+def hello_t(array)
+  if block_given?
+    i = 0
+    newArray = []
 
+    while i < array.length
+      if array[i].start_with?('T') || array[i].start_with?('t')
+        newArray << array[i]
+        yield(array[i])
+      end
+      i = i + 1
+    end
+    newArray
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?('T') || name.start_with?('t')
+    puts "Hi, #{name}"
+  end
+end
