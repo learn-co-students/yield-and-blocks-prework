506016e3945346378808d8e79313212d82507ede
Logan Emerson <logemerson@gmail.com>
2017-07-04 17:49:59 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,22 @@
-def hello_t
+require 'pry'
 
+def hello_t(array)
+  i = 0
+  new_array = []
+
+  while i < array.length
+    if array[i].start_with?("T") || array[i].start_with?("t")
+      yield array[i]
+      new_array.push(array[i])
+    end
+    i += 1
+  end
+  
+  new_array
 end
 
 # call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  puts "Hi, #{name}"
 
+end
