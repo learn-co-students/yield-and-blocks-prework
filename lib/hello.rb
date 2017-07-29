8b6145424bea2913f22386e47430d5c80b0601ca
xxDOOMbox <shawwian@gmail.com>
2017-07-29 17:48:54 -0600

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,7 @@
-def hello_t
-
+def hello_t(names)
+	names.map {|name| yield(name) if name.downcase.start_with?("t") }
+	names.select { |name| name.downcase.start_with?("t") }
 end
 
 # call your method here!
-
+hello_t(["tim", "tam", "tom", "joe"]) { |name| puts "Hi, #{name}" }  
