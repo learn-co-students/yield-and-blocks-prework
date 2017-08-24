55c9046c984fae2294745dcaa24cf391d51c6cdd
gene-yoo <gene.y.yoo@gmail.com>
2017-08-24 16:23:52 -0400

    Lab complete.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,8 @@
-def hello_t
-
+def hello_t(array)
+  array.select do |name|
+    if (name.start_with?("T") || name.start_with?("t"))
+      yield name
+      true
+    end
+  end
 end
-
-# call your method here!
-
