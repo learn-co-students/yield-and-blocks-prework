50baa4de41404a75f401cfb9f07ad8fcbf84a711
Bryan Teng <bteng1226@gmail.com>
2018-06-24 20:36:03 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
 def hello_t(array)
   return puts("Hey! No block was given!") if array.length == 0
-array.each{|x| puts x}
+index = 0
+while index < array.length
+yield[index]
+index +=1
+end
+
 end
 
 # call your method here!
