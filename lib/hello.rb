74ed654ececcdf7e7d158d4deb284e271560b9b3
Bryan Teng <bteng1226@gmail.com>
2018-06-24 20:41:42 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,13 @@
 def hello_t(array)
-puts("Hey! No block was given!") if array.length == 0
+if !block_given?
+  puts("Hey! No block was given!")
+else
 index = 0
 while index < array.length
 yield(array[index])
 index +=1
 end
-
+end
 array
 end
 
