7cda891a934782817dff5592c29693c86acb9781
Bryan Teng <bteng1226@gmail.com>
2018-06-24 20:42:18 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,14 @@
 def hello_t(array)
-if !block_given?
-  puts("Hey! No block was given!")
-else
-index = 0
-while index < array.length
-yield(array[index])
-index +=1
-end
-end
-array
+  if !block_given?
+    puts("Hey! No block was given!")
+  else
+    index = 0
+    while index < array.length
+      yield(array[index])
+      index += 1
+    end
+  end
+  array
 end
 
 # call your method here!
