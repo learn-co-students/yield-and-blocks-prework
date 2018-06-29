ca188c329628fd49e26ed520433041fd7aabb54b
github username <github email address>
2018-06-29 15:42:14 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,13 +8,8 @@ def hello_t(array)
     array
   else
     puts "Hey! No block was given!"
-  end 
+  end
 end
 
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
