eec16cc38fdc59550447c05b597f10c1d719f5e5
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-07 15:14:00 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,18 @@
-def hello_t
-
+def hello_t(array)
+  if block_given? == false
+    puts "Hey! No block was given!"
+    return array
+  end
+  array.each do |el|
+    yield el
+  end
+  array
 end
 
 # call your method here!
 
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
