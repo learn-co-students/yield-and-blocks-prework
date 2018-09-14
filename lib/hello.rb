e2e6090d7f1e5627cb07c86439c5472937810d2e
Amir Safoev <amirsafoev0@gmail.com>
2018-09-14 16:12:06 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,6 +4,10 @@ while i < array.length
 yield array[i]
 i = i + 1 
 end 
+array
+else
+    puts "Hey! No block was given!"
+  end
 end
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
