53d3f014ab96bdf1f112618de3f549712b701d10
Tony J. Lum <Tonyjlum@gmail.com>
2018-10-25 02:16:46 -0400

    Yield

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,10 @@
-def hello_t
-
-end
-
 # call your method here!
-
+def hello_t(array)
+  if block_given?
+    array.each do |el|
+      yield(el)
+    end
+  else
+    puts "Hey! No block was given!"
+  end
+end
