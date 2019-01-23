b000d79c053a0c95b96934c5b519fd646b27a00f
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-01-23 13:52:43 -0600

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,23 @@
-def hello_t
 
-end
+  def hello_t(array)
+    if block_given?
+      i = 0
+
+      while i < array.length
+        yield(array[i])
+        i = i + 1
+      end
+
+      array
+    else
+      puts "Hey! No block was given!"
+    end
+  end
 
 # call your method here!
 
+
+other_names = ["Ram","Sam","Tim"]
+hello_t(other_names) do |name|
+  names = other_names.each {|i| puts i.upcase}
+end
