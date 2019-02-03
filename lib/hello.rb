62051e946d7f9c692f60adb78fb9b05f4dbf0511
es6ix <46784380+es6ix@users.noreply.github.com>
2019-02-03 18:12:35 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
-def hello_t
+def hello_t(names)
+    if block_given?
+        names.each do |name|
+            yield(name)
+        end
 
+        names
+    else 
+        puts "Hey! No block was given!"
+    end
 end
-
-# call your method here!
-
